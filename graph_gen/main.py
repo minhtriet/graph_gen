40972630bc546380da592d0b52843d1c8ceda83e
minhtriet <minhtriet09@gmail.com>
2026-03-16 23:17:32 +0000

    init cm

--- graph_gen/main.py
+++ graph_gen/main.py
@@ -1,22 +1,23 @@
-import jax
-from flax import linen as nn
+import torch
+from torch import nn
 
 
 class GCNLayer(nn.Module):
-    c_out: int  # Output feature size
+    def __init__(self, c_in, c_out):
+        super().__init__()
+        self.projection = nn.Linear(c_in, c_out)
 
-    @nn.compact
-    def __call__(self, node_feats, adj_matrix):
+    def forward(self, node_feats, adj_matrix):
         """
         Inputs:
-            node_feats - Array with node features of shape [batch_size, num_nodes, c_in]
+            node_feats - Tensor with node features of shape [batch_size, num_nodes, c_in]
             adj_matrix - Batch of adjacency matrices of the graph. If there is an edge from i to j, adj_matrix[b,i,j]=1 else 0.
                          Supports directed edges by non-symmetric matrices. Assumes to already have added the identity connections.
                          Shape: [batch_size, num_nodes, num_nodes]
         """
         # Num neighbours = number of incoming edges
-        num_neighbours = adj_matrix.sum(axis=-1, keepdims=True)
-        node_feats = nn.Dense(features=self.c_out, name="projection")(node_feats)
-        node_feats = jax.lax.batch_matmul(adj_matrix, node_feats)
+        num_neighbours = adj_matrix.sum(dim=-1, keepdims=True)
+        node_feats = self.projection(node_feats)
+        node_feats = torch.bmm(adj_matrix, node_feats)
         node_feats = node_feats / num_neighbours
         return node_feats
