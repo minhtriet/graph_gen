d6d73ef5088d28d678c43fe3cfccf7b6ec3f7c1e
cmt <minhtriet09@gmail.com>
2026-03-17 16:39:47 +0000

    add some draft

--- graph_gen/main.py
+++ graph_gen/main.py
@@ -2,22 +2,27 @@ import torch
 from torch import nn
 
 
+
 class GCNLayer(nn.Module):
     def __init__(self, c_in, c_out):
         super().__init__()
         self.projection = nn.Linear(c_in, c_out)
 
-    def forward(self, node_feats, adj_matrix):
-        """
-        Inputs:
-            node_feats - Tensor with node features of shape [batch_size, num_nodes, c_in]
-            adj_matrix - Batch of adjacency matrices of the graph. If there is an edge from i to j, adj_matrix[b,i,j]=1 else 0.
-                         Supports directed edges by non-symmetric matrices. Assumes to already have added the identity connections.
-                         Shape: [batch_size, num_nodes, num_nodes]
-        """
-        # Num neighbours = number of incoming edges
-        num_neighbours = adj_matrix.sum(dim=-1, keepdims=True)
-        node_feats = self.projection(node_feats)
-        node_feats = torch.bmm(adj_matrix, node_feats)
-        node_feats = node_feats / num_neighbours
-        return node_feats
+    def forward(self, node_feeats, adj_matrix):
+        e_st, e_end = b.edges[:,0], b.edges[:,1]
+        dists = torch.norm(b.x[e_st] - b.x[e_end], dim=1).reshape(-1, 1)
+        
+        # compute messages
+        tmp = torch.hstack([b.h[e_st], b.h[e_end], dists])
+        m_ij = self.f_e(tmp)
+        
+        # predict edges
+        e_ij = self.f_inf(m_ij)
+        
+        # average e_ij-weighted messages  
+        # m_i is num_nodes x hid_dim
+        m_i = index_sum(b.h.shape[0], e_ij*m_ij, b.edges[:,0], self.cuda)
+        
+        # update hidden representations
+        b.h += self.f_h(torch.hstack([b.h, m_i]))
+
