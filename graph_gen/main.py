5c693bf502b5ac5e978a51878966a39c1e6cd46e
cmt <minhtriet09@gmail.com>
2026-03-18 21:30:10 +0000

    draft forward encode pass, no vectorization

--- graph_gen/main.py
+++ graph_gen/main.py
@@ -3,12 +3,14 @@ from torch import nn
 
 
 class ECNN(nn.Module):
-    def __init__(self, n_node, dim_node_e):
+    def __init__(self, M, n):
         """
-        The definition are from Appendix C
+        The definition are from Appendix C https://arxiv.org/pdf/2102.09844
+        M: Number of nodes
+        n: dimension of the node
         """
         super().__init__()
-        self.h = nn.Embedding(n_node, dim_node_e)
+        self.h = nn.Embedding(M, n)
         # edge function
         self.phi_e = nn.Sequential(
             nn.Linear(in_features, hidden_features),
@@ -27,11 +29,25 @@ class ECNN(nn.Module):
             nn.SiLU(),
             nn.Linear(hidden_features, out_features),
         )
+        complete_graph = torch.one((M, M))
+        self_loop = torch.I(M)
+        self.all_neighbors = complete_graph - self_loop
     
     def forward(self, batch):
+        """
+        batch: [:, x, a]
+        a: edge attribute
+        x: coordinate embedding
+        h: node embedding
+        M : number of nodes
+        """
+        C = 1 / (self.M - 1)
         # encoder
-        dist = torch.norm()
-
+        dist = torch.norm(batch(x[i] - x[j]))
+        m_ij = self.phi_e(self.h)
+        x_i = xi + C*(x_i - x_j)*self.phi_x(m_ij)
+        m_i = m_ij @ self.all_neighbors
+        h = phi_h(h_i, m_i)
         # decoder
         pass
 
